0658edc20c16546c90efbd3f658ff94fad29372a
brenogab <115730391+brenogab@users.noreply.github.com>
2024-01-21 13:20:35 -0300

    Include Django Admin customizations

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,55 @@
+"""
+Test for the Django Admin modifications.
+"""
+
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    """ Test for Django admin """
+
+    def setUp(self):
+        """ Create User and Client """
+
+        self.client = Client()
+
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='password'
+        )
+
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='password',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        """ Test that users are listed in the page """
+
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test edit user page exists """
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """ Test Creating User Page works """
+
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
